40973d27fac1f48110ae9e8a6f2dfe08a3cd8c4c
fati-ma <fatima.atiyya@yahoo.com>
2021-02-03 22:05:32 +0200

    class-29

---
+++ resty-app/src/components/Help.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Help () {
+    return (
+        <>
+            <h1>How To play with RESTy</h1>
+            
+            <h3>Enter the URL</h3>
+            <h3>Select a method</h3>
+            <h3>If the method is POST or PUT enter a body in JSON format</h3>
+            <h3>Hit GO</h3>
+            <h3>To see the history click on History link</h3>
+        </>
+    )
+}
